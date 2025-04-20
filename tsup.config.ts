e88266cee0c52322ae684506ec6a42e083656363
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2025-04-20 00:23:36 -0500

    [ADD] tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"], // Cambia esto según la ubicación de tu archivo principal
+  format: ["esm", "cjs"], // Formatos de salida (ESM y CommonJS)
+  dts: true, // Genera los archivos de declaración .d.ts
+  sourcemap: true, // Genera mapas de origen
+  clean: true, // Limpia la carpeta de salida antes de generar
+  outDir: "dist", // Carpeta de salida
+});
